870e6bb18ef3179889b1178fe02c701f7e3bbe73
kotORsova <kat5ya2301@gmail.com>
2023-03-31 19:12:32 +0300

    add packages, component

---
+++ Module2-Annotations/src/main/java/org/example/main/Main.java
@@ -0,0 +1,14 @@
+package org.example.main;
+
+import org.example.config.ProjConf;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjConf.class);
+        Parrot p1 = context.getBean(Parrot.class);
+        p1.setName("Gosha");
+        System.out.println(p1);
+        System.out.println(p1.getName());
+    }
+}
