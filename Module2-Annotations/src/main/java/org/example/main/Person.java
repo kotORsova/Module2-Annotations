9e2e9c5b38a34fc3dcecd7021f3b7b5cfb58346f
kotORsova <kat5ya2301@gmail.com>
2023-04-05 10:32:02 +0300

    add new component class person

---
+++ Module2-Annotations/src/main/java/org/example/main/Person.java
@@ -0,0 +1,30 @@
+package org.example.main;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name;
+    private Parrot parrot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    @Override
+    public String toString() {
+        return "Person's parrot: " + parrot;
+    }
+}
